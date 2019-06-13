538e5bc0634f8a10dc2bbfd4d4969ec48f1be6dc
Jun Kim <93kimhyunjun@gmail.com>
2019-06-13 23:14:22 +0900

    Add a recording function

---
+++ assets/js/videoRecorder.js
@@ -0,0 +1,57 @@
+const recorderContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
+
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = event => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Stop recording";
+  streamObject.stop();
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const getVideo = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 }
+    });
+    videoPreview.srcObject = stream;
+    videoPreview.muted = true;
+    videoPreview.play();
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
+  } catch (error) {
+    recordBtn.innerHTML = "😭 Cant record";
+  } finally {
+    recordBtn.removeEventListener("click", startRecording);
+  }
+};
+
+function init() {
+  recordBtn.addEventListener("click", getVideo);
+}
+
+if (recorderContainer) {
+  init();
+}
